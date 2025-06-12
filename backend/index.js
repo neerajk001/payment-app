0f12aa864934a48c9dd6532b8976bcc427104ff5
neeraj kushwaha <workwithneeraj.01@gmail.com>
2025-06-12 18:05:38 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: ['https://payment-app-dacc.vercel.app/'], 
+  origin: ['http://localhost:5173', 'https://payment-app-dacc.vercel.app'],
   credentials: true
 }));
 
