c12f84d10cfb2440bbef3abe42d656f4c5caf3e2
neeraj kushwaha <workwithneeraj.01@gmail.com>
2025-06-12 22:26:53 +0530

    added padding in btn

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({label, onClick}) => {
   return (
     <div className='mt-4 flex'>
         <button onClick={onClick} 
-         type='button' className='w-full bg-blue-700 hover:bg-blue-900 py-0.5 text-white rounded text-md hover:cursor-pointer'>
+         type='button' className='w-full bg-blue-700 hover:bg-blue-900 py-1 px-1.5 text-white rounded text-md hover:cursor-pointer'>
             {label}
         </button>
     </div>
